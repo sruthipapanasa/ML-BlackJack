37376195363dd6ce2d5b262622d801510e34d39e
Misa Franknedy <mfrankne@ucsd.edu>
2020-08-29 23:41:41 +0000

    added tensorflow stuff

--- main.py
+++ main.py
@@ -1,22 +1,63 @@
-#hi hoes, you need to figure out how to clear the player hand
-#also add if statements if user repeats or misspells data
-#win / lose triggers
-#train our function
-
-
-
-""""
-import nltk 
-import pandas as pd
-from nltk.classify import NaiveBayesClassifier
-from nltk.classify.util import accuracy
-import math
-
-
-data = pd.read_csv("blkjckhands.csv") """
-
+import os
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import csv
 import random
 
+
+print("TensorFlow version: {}".format(tf.__version__))
+print("Eager execution: {}".format(tf.executing_eagerly()))
+
+# column order in CSV file
+column_names = ['index', 'player_number', 'card1', 'card2', 'card3', 'card4', 'card5', 'cardsum', 'dcard1', 'dcard2', 'dcard3', 'dcard4', 'dcard5', 'dcardsum', 'blkjck', 'winloss', 'plybustbeat', 'dlbustbeat', 'plwinamt', 'dlwinamt', 'ply2cardsum']
+
+feature_names = column_names[:-1]
+label_name = column_names[-1]
+print("Features: {}".format(feature_names))
+print("Label: {}".format(label_name))
+
+blackjackFile = "blkjckhands.csv"
+with open(blackjackFile, 'r') as csvfile:
+    pointreader = csv.reader(csvfile)
+""""
+    next(pointreader)
+    for i in range (10):
+        for row in pointreader:
+            pass
+            #print(row[2]) """
+
+
+batch_size = 1000
+train_dataset = tf.data.experimental.make_csv_dataset(
+    blackjackFile,
+    batch_size,
+    column_names=column_names,
+    label_name=label_name,
+    num_epochs=1)
+
+features, labels = next(iter(train_dataset))
+
+for f in features['winloss'].numpy():
+    if f == "Win":
+        f = 1
+    else:
+        f = 0
+
+print('Features: ')
+print(features['winloss'].numpy())
+
+plt.scatter(features['card1'],
+            features['cardsum'],
+            c=labels,
+            cmap='viridis')
+
+plt.xlabel("Card 1")
+plt.ylabel("Card Sum")
+plt.show()
+
+
+
+'''
 #setting up card
 class Card:
     def __init__(self, suit, rank, value=0):
@@ -27,10 +68,6 @@ class Card:
     def show(self):
         print("{} of {} ({})".format(self.rank, self.suit, self.value))
 
-    def clear(self):
-        self.suit = ''
-        self.rank = ''
-        self.value = 0
 
 class Deck:
     def __init__(self):
@@ -50,22 +87,19 @@ class Deck:
         for i in range(len(self.cards)-1, 0, -1):
             r = random.randint(0, i)
             self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
-
-    def clear(self):
-        for c in self.cards:
-            c.clear()
             
     def show(self):
         for c in self.cards:
             c.show()
 
-#match parameters given to card in deck
+#match parameters given to card in deck and returns card
 def card_updater(deck, card_rank, card_suit):
     for card in deck.cards:
         if card.rank == card_rank:
             if card.suit == card_suit:
                 return card
 
+#hi-lo counter used in blackjack games
 def counter(played_cards, count = 0):
     for card in played_cards.cards:
         if 2 <= card.value <= 6:
@@ -85,7 +119,7 @@ def prediction (count, player_hand):
     print("The sum of your cards are:", sum)
 
     print("The current hi-lo count is:", str(count))
-    if sum < 12:
+    if sum <=12:
         print('You should draw. Value of hand is low.')
     else:
         if count > 0:
@@ -96,34 +130,45 @@ def prediction (count, player_hand):
             print("Count is low enough. Feel free to draw")
 
 def blackjack_setup(played_cards, player_hand):
-    player_hand.clear()
-    
    #card 1 information
     card1_rank = input("Enter your first card's rank.\n")
     card1_suit = input("Enter your first card's suit. \n")
+    card1 = card_updater(deck, card1_rank, card1_suit)
+    if card1 not in deck.cards:
+        print("Your card cannot be found in the deck. Please try again.")
+        blackjack_setup(played_cards, player_hand)
+    else:
+        played_cards.cards.append(card1)
+        player_hand.cards.append(card1)
+        deck.cards.remove(card1)
     
     #card 2 information
     card2_rank = input("Enter your second card's rank.\n")
     card2_suit = input("Enter your second card's suit. \n")
+    card2 = card_updater(deck, card2_rank, card2_suit)
+    while card2 not in deck.cards:
+        print("Your card cannot be found in the deck. Please try again.\n")
+        card2_rank = input("Enter your second card's rank.\n")
+        card2_suit = input("Enter your second card's suit. \n")
+        card2 = card_updater(deck, card2_rank, card2_suit)
+    else:
+        played_cards.cards.append(card2)
+        player_hand.cards.append(card2)
+        deck.cards.remove(card2)
 
     #dealer information
     dcard_rank = input("Enter the dealer card rank.\n")
     dcard_suit = input("Enter the dealer card suit. \n")
-    
-    #remove cards that have already been dealt
-    card1 = card_updater(deck, card1_rank, card1_suit)
-    played_cards.cards.append(card1)
-    player_hand.cards.append(card1)
-    deck.cards.remove(card1)
-    
-    card2 = card_updater(deck, card2_rank, card2_suit)
-    played_cards.cards.append(card2)
-    player_hand.cards.append(card2)
-    deck.cards.remove(card2)
-    
     dealer = card_updater(deck, dcard_rank, dcard_suit)
-    played_cards.cards.append(dealer)
-    deck.cards.remove(dealer)
+    while dealer not in deck.cards:
+        print("Your card cannot be found in the deck. Please try again.\n")
+        dcard_rank = input("Enter the dealer card rank.\n")
+        dcard_suit = input("Enter the dealer card suit. \n")
+        dealer = card_updater(deck, dcard_rank, dcard_suit)
+    else:
+        played_cards.cards.append(dealer)
+        player_hand.cards.append(dealer)
+        deck.cards.remove(dealer)
 
     count2 = counter(played_cards)
     prediction (count2, player_hand)
@@ -134,9 +179,15 @@ def blackjack_setup(played_cards, player_hand):
         newcard_rank = input("Enter the new card rank.\n")
         newcard_suit = input("Enter the new card suit. \n")
         newcard = card_updater(deck, newcard_rank, newcard_suit)
-        played_cards.cards.append(newcard)
-        player_hand.cards.append(newcard)
-        deck.cards.remove(newcard)
+        while newcard not in deck.cards:
+            print("Your card cannot be found in the deck. Please try again.\n")
+            newcard_rank = input("Enter the new card rank.\n")
+            newcard_suit = input("Enter the new card suit. \n")
+            newcard = card_updater(deck, newcard_rank, newcard_suit)
+        else:
+            played_cards.cards.append(newcard)
+            player_hand.cards.append(newcard)
+            deck.cards.remove(newcard)
         counter3 = counter(played_cards)
         prediction(counter3, player_hand)
         answer = input('\nAre you going to draw again? Enter Y or N.\n')
@@ -162,5 +213,6 @@ blackjack_setup(played_cards, player_hand)
 print("played cards:")
 played_cards.show()
 print("player hand")
-player_hand.cards.show()
+player_hand.show()
 
+'''
